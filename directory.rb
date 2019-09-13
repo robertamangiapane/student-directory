4dd3eb690c1edc52bfce47d38dec68fd0ab697e2
Roberta Mangiapane <roberta.mangiapane@gmail.com>
2019-09-13 12:39:36 +0000

    Using CSV Library

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 @students = []
 require 'date'
+require 'csv'
 @cohorts = Date::MONTHNAMES.compact
 
 def interactive_menu
@@ -12,8 +13,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from a file"
   puts "9. Exit"
 end
 
@@ -26,17 +27,18 @@ def process(selection)
     when "3"
       save_students
     when "4"
-      load_students
+      puts "Type the name of the file you want to load"
+      filename = STDIN.gets.chomp
+      load_students("#{filename}.csv")
     when "9"
       exit 
-      #this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
   end
 end    
 
 def input_students
-  puts "Please enter the names of the students, hobbies, age, country of birth and cohort"
+  puts "Please enter the names of the student"
   puts "To finish, just hit return twice"
   name = STDIN.gets.chomp
   while !name.empty? do
@@ -47,7 +49,7 @@ def input_students
     end
     puts "Enter age: "
     age = STDIN.gets.chomp
-    if !age.is_a? Integer
+    if age.to_i == 0
       age = "N/A"
     end
     puts "Enter country of birth: "
@@ -68,9 +70,9 @@ def input_students
     end  
     @students << {name: name, hobby: hobby, age: age, cob: cob, cohort: cohort}
     if @students.count == 1
-      puts "Now we have #{@students.count} student"
+      puts "Now we have #{@students.count} student in the list"
     else
-      puts "Now we have #{@students.count} students"
+      puts "Now we have #{@students.count} students in the list"
     end
     puts "Enter students' name"
     name = STDIN.gets.chomp
@@ -88,34 +90,46 @@ def show_students
 end 
 
 def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:hobby], student[:age], student[:cob], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  if @students.count != 0
+    puts "Type the name of the file you want to save"
+    filename = STDIN.gets.chomp
+    CSV.open("#{filename}.csv", "wb") do |file|
+      @students.each do |student|
+        student_data = [student[:name], student[:hobby], student[:age], student[:cob], student[:cohort]]
+        file << student_data
+      end
+    end
+    puts "You saved the students' list"
+  else
+    puts "There is no list to save"
   end
-  file.close
 end
 
 def try_load_students
-  filename = ARGV.first
+  filename = "students.csv"
   return if filename.nil?
-  if File.exists?(filename)
+  if File.exist?(filename)
     load_students(filename)
       puts "Loaded #{@students.count} from #{filename}"
   else
-    puts "Sorry, #{filename} doesn't exist."
-    exit
+    puts "Sorry, tried to load #{filename} but it doesn't exist."
   end
 end
 
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, hobby, age, cob, cohort = line.chomp.split(",")
-      @students << {name: name, hobby: hobby, age: age, cob: cob, cohort: cohort}
+def load_students(filename)
+  if File.exist?(filename)
+    CSV.foreach(filename) do |line|
+      name, hobby, age, cob, cohort = line
+        @students << {name: name, hobby: hobby, age: age, cob: cob, cohort: cohort}
+    end
+    if @students.count != 1
+      puts "Loaded #{@students.count} students from #{filename}"
+    else
+      puts "Loaded #{@students.count} student from #{filename}"
+    end
+  else
+    puts "Sorry, #{filename} doesn't exist."
   end
-  file.close
 end
   
 def print_header
